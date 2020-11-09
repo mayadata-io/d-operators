c5fee8430677de39faf8e6a2ed9ea2da19dfb151
Amit Kumar Das <amit.das@mayadata.io>
2020-11-09 16:06:23 +0530

    feat(crd): add v1 apis to apply CRDs (#178)
    Signed-off-by: AmitKumarDas <amit.das@mayadata.io>

--- pkg/recipe/apply.go
+++ pkg/recipe/apply.go
@@ -18,8 +18,10 @@ package recipe
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/pkg/errors"
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -50,6 +52,54 @@ func NewApplier(config ApplyableConfig) *Applyable {
 		Apply:      config.Apply,
 		result:     &types.ApplyResult{},
 	}
+}
+
+func (a *Applyable) postCreateCRDV1(crd *v1.CustomResourceDefinition) error {
+	if len(crd.Spec.Versions) == 0 {
+		return errors.Errorf(
+			"Invalid CRD spec: Missing spec.versions",
+		)
+	}
+	var versionToVerify = crd.Spec.Versions[0].Name
+	message := fmt.Sprintf(
+		"PostCreate CRD: Kind %s: APIVersion %s",
+		crd.Spec.Names.Singular,
+		crd.Spec.Group+"/"+versionToVerify,
+	)
+	// Is custom resource definition discovered &
+	// can its resource(s) be listed
+	err := a.Retry.Waitf(
+		func() (bool, error) {
+			got := a.GetAPIForAPIVersionAndResource(
+				crd.Spec.Group+"/"+versionToVerify,
+				crd.Spec.Names.Plural,
+			)
+			if got == nil {
+				return a.IsFailFastOnDiscoveryError(),
+					errors.Errorf(
+						"Failed to discover: Kind %s: APIVersion %s",
+						crd.Spec.Names.Singular,
+						crd.Spec.Group+"/"+versionToVerify,
+					)
+			}
+			// fetch dynamic client for the custom resource
+			// corresponding to this CRD
+			customResourceClient, err := a.GetClientForAPIVersionAndResource(
+				crd.Spec.Group+"/"+versionToVerify,
+				crd.Spec.Names.Plural,
+			)
+			if err != nil {
+				return a.IsFailFastOnDiscoveryError(), err
+			}
+			_, err = customResourceClient.List(metav1.ListOptions{})
+			if err != nil {
+				return false, err
+			}
+			return true, nil
+		},
+		message,
+	)
+	return err
 }
 
 func (a *Applyable) postCreateCRD(
@@ -93,6 +143,54 @@ func (a *Applyable) postCreateCRD(
 		message,
 	)
 	return err
+}
+
+func (a *Applyable) createCRDV1() (*types.ApplyResult, error) {
+	var crd *v1.CustomResourceDefinition
+	err := UnstructToTyped(a.Apply.State, &crd)
+	if err != nil {
+		return nil, err
+	}
+	// use crd client to create crd
+	crd, err = a.crdClientV1.
+		CustomResourceDefinitions().
+		Create(crd)
+	if err != nil {
+		return nil, err
+	}
+	// add to teardown functions
+	a.AddToTeardown(func() error {
+		_, err := a.crdClientV1.
+			CustomResourceDefinitions().
+			Get(
+				crd.GetName(),
+				metav1.GetOptions{},
+			)
+		if err != nil && apierrors.IsNotFound(err) {
+			// nothing to do
+			return nil
+		}
+		return a.crdClientV1.
+			CustomResourceDefinitions().
+			Delete(
+				crd.Name,
+				nil,
+			)
+	})
+	// run an additional step to wait till this CRD
+	// is discovered at apiserver
+	err = a.postCreateCRDV1(crd)
+	if err != nil {
+		return nil, err
+	}
+	return &types.ApplyResult{
+		Phase: types.ApplyStatusPassed,
+		Message: fmt.Sprintf(
+			"Create CRD: Kind %s: APIVersion %s",
+			crd.Spec.Names.Singular,
+			a.Apply.State.GetAPIVersion(),
+		),
+	}, nil
 }
 
 func (a *Applyable) createCRD() (*types.ApplyResult, error) {
@@ -139,6 +237,66 @@ func (a *Applyable) createCRD() (*types.ApplyResult, error) {
 			"Create CRD: Kind %s: APIVersion %s",
 			crd.Spec.Names.Singular,
 			crd.Spec.Group+"/"+crd.Spec.Version,
+		),
+	}, nil
+}
+
+func (a *Applyable) updateCRDV1() (*types.ApplyResult, error) {
+	var crd *v1.CustomResourceDefinition
+	// transform to typed CRD to make use of crd client
+	err := UnstructToTyped(a.Apply.State, &crd)
+	if err != nil {
+		return nil, err
+	}
+	// get the CRD observed at the cluster
+	target, err := a.crdClientV1.
+		CustomResourceDefinitions().
+		Get(
+			a.Apply.State.GetName(),
+			metav1.GetOptions{},
+		)
+	if err != nil {
+		return nil, err
+	}
+	// tansform back to unstruct type to run 3-way merge
+	targetAsUnstruct, err := TypedToUnstruct(target)
+	if err != nil {
+		return nil, err
+	}
+	merged := &unstructured.Unstructured{}
+	// 3-way merge
+	merged.Object, err = dynamicapply.Merge(
+		targetAsUnstruct.UnstructuredContent(), // observed
+		a.Apply.State.UnstructuredContent(),    // last applied
+		a.Apply.State.UnstructuredContent(),    // desired
+	)
+	if err != nil {
+		return nil, err
+	}
+	// transform again to typed CRD to execute update
+	err = UnstructToTyped(merged, crd)
+	if err != nil {
+		return nil, err
+	}
+	// update the final merged state of CRD
+	//
+	// NOTE:
+	//	At this point we are performing a server side
+	// apply against the CRD
+	_, err = a.crdClientV1.
+		CustomResourceDefinitions().
+		Update(
+			crd,
+		)
+	if err != nil {
+		return nil, err
+	}
+	return &types.ApplyResult{
+		Phase: types.ApplyStatusPassed,
+		Message: fmt.Sprintf(
+			"Update CRD: Kind %s: APIVersion %s",
+			crd.Spec.Names.Singular,
+			a.Apply.State.GetAPIVersion(),
 		),
 	}, nil
 }
@@ -203,12 +361,61 @@ func (a *Applyable) updateCRD() (*types.ApplyResult, error) {
 	}, nil
 }
 
+func (a *Applyable) applyCRDV1() (*types.ApplyResult, error) {
+	var crd *v1.CustomResourceDefinition
+	err := UnstructToTyped(a.Apply.State, &crd)
+	if err != nil {
+		return nil, err
+	}
+	message := fmt.Sprintf(
+		"Apply CRD: Kind %s: APIVersion %s",
+		crd.Spec.Names.Singular,
+		a.Apply.State.GetAPIVersion(),
+	)
+	// use crd client to get crd
+	err = a.Retry.Waitf(
+		func() (bool, error) {
+			_, err = a.crdClientV1.
+				CustomResourceDefinitions().
+				Get(
+					crd.GetName(),
+					metav1.GetOptions{},
+				)
+			if err != nil {
+				if apierrors.IsNotFound(err) {
+					// condition exits since this is valid
+					return true, err
+				}
+				return false, err
+			}
+			return true, nil
+		},
+		message,
+	)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			// this is a **create** operation
+			return a.createCRDV1()
+		}
+		return nil, err
+	}
+	// this is an **update** operation
+	return a.updateCRDV1()
+}
+
 func (a *Applyable) applyCRD() (*types.ApplyResult, error) {
+	ver := a.Apply.State.GetAPIVersion()
+	if strings.HasSuffix(ver, "/v1") {
+		return a.applyCRDV1()
+	}
+
 	var crd *v1beta1.CustomResourceDefinition
 	err := UnstructToTyped(a.Apply.State, &crd)
 	if err != nil {
 		return nil, err
 	}
+
+	// following code belongs to v1beta1 version
 	message := fmt.Sprintf(
 		"Apply CRD: Kind %s: APIVersion %s",
 		crd.Spec.Names.Singular,
